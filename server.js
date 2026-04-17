b964cdcb5dfe48add952778830b11c2ff86b3d9b
bartnowicki85-ux <bartnowicki85@gmail.com>
2026-04-17 02:08:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import fs from "fs";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Justyna AI działa 💇‍♀️");
